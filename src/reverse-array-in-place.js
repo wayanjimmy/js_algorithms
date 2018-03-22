754d7f0990f32073aaea99d1dd0b733a24c97126
Wayan Jimmy <jimmyeatcrab@gmail.com>
2018-03-22 16:59:37 +0700

    more efficient algorithm

--- src/reverse-array-in-place.js
+++ src/reverse-array-in-place.js
@@ -1,14 +1,9 @@
 function reverseArrayInPlace(arr) {
-  for (let j = 1; j < arr.length; j++) {
-    let temp = arr[0]
-
-    for (let i = 0; i < arr.length - j; i++) {
-      arr[i] = arr[i + 1]
-    }
-
-    arr[arr.length - j] = temp
+  for (let i = 0; i < arr.length / 2; i++) {
+    let temp = arr[i]
+    arr[i] = arr[arr.length - 1 - i]
+    arr[arr.length - 1 - i] = temp
   }
-
   return arr
 }
 
