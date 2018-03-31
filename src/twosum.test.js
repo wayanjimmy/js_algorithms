c8b6ff37a5e17cedc0b85594c4c716e80429d00e
Wayan Jimmy <jimmyeatcrab@gmail.com>
2018-03-31 21:15:24 +0800

    add test cast for twosum

--- src/twosum.test.js
+++ src/twosum.test.js
@@ -1,5 +1,9 @@
 const twoSum = require('./twosum')
 
 it('should return [[6,1], [3,4], [3,4]]', () => {
-  expect(twoSum([1, 6, 4, 5, 3, 3], 7)).toEqual([[6,1], [3,4], [3,4]])
+  expect(twoSum([1, 6, 4, 5, 3, 3], 7)).toEqual([[6, 1], [3, 4], [3, 4]])
+})
+
+it('should return [[17,11], [-12,40]]', () => {
+  expect(twoSum([40, 11, 19, 17, -12], 28)).toEqual([[17, 11], [-12, 40]])
 })
